05c0245ceac6c99383f466898f23a64b608e4108
treyzania <treyzania@gmail.com>
2017-12-16 23:08:04 -0500

    Changed argument patterns to include richer data about the remote host.

--- client/main.rs
+++ client/main.rs
@@ -11,15 +11,15 @@ fn main() {
     let matches = clap_app!(myapp =>
         (version: "0.1.0")
         (author: "treyzania <treyzania@gmail.com>")
-        (about: "Opportunistically file trees between multiple computers to a remote server.")
-        (@arg INPUT: +required "Watched directory.")
-        (@arg DATA: +required "Data directory, data for pending files are stored.")
-        (@arg DEST: +required "Destination expression, passed directly to rsync.  Password-less login required!"))
+        (about: "Opportunistically syncs file trees between multiple computers to a remote server.")
+        (@arg remote_user: -c +takes_value "Override the username to connect to the remote host as.")
+        (@arg remote_path: -p +takes_value "Override the remote path to store data in.  Default: ~/.synmux")
+        (@arg label: +required "Label for this sync instance.")
+        (@arg watch: +required "Watched directory.")
+        (@arg rhost: +required "Remote host address."))
         .get_matches();
 
-    let dir = matches.value_of("INPUT").unwrap();
-    let _data = matches.value_of("DATA").unwrap();
-    let _dest = matches.value_of("DEST").unwrap();
+    let dir = matches.value_of("watch").unwrap();
 
     watch::dir_watch_prodecure(vec![PathBuf::from(dir)]);
 
